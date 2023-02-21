4fdbd133a821636548e1c97ddf4ecc10e6269eb8
xuchengli <lixucheng@aliyun.com>
2023-02-21 19:22:15 +0800

    emit cpu usage by web socket

--- ui/app/io/middleware/connection.js
+++ ui/app/io/middleware/connection.js
@@ -1,6 +1,19 @@
+'use strict';
+
+const { spawn } = require('child_process');
+
 module.exports = (app) => {
     return async (ctx, next) => {
-        ctx.socket.emit('res', 'connected!');
+        const top = spawn(`top -pid $(ps -ef | grep -v grep | grep simd | awk '{print $2}') -stats cpu`, {
+            shell: true,
+        });
+        top.stdout.on('data', data => {
+            const cpu = data.toString().split('\n')[12];
+            
+            console.log('cpu使用率: ', cpu);
+
+            ctx.socket.emit('res', cpu);
+        });
         await next();
         // execute when disconnect.
         console.log('disconnection!');
